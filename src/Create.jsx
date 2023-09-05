df431373bda6dd201a38763c187f14a9ca638b6b
Dagim Asnake <dagimasnake1@gmail.com>
2023-09-05 10:16:59 -1200

    finish create

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { addUser } from './UserReducer'
+import {useNavigate} from 'react-router-dom'
 
 const Create = () => {
+    const navigate = useNavigate()
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
+    const dispatch = useDispatch()
+    const users = useSelector((state) => state.users)
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(addUser({id: users[users.length - 1].id + 1, name: name, email: email}))
+        navigate('/')
+    }
   return (
-    <div>Create</div>
+    <div className="d-flex w-100 vh-100 justify-content-center align-items-center">
+        <div className="w-50 border bg-secondary text-white p-5">
+            <h2>Add New User</h2>
+            <form onSubmit={submitHandler}>
+                <div>
+                    <label htmlFor="name"> Name </label>
+                    <input type="text" name='name' className='form-control' onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name='email' className='form-control' onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <br />
+                <button className='btn btn-info'>Submit</button>
+            </form>
+        </div>
+    </div>
   )
 }
 
--- src/UserReducer.jsx
+++ src/UserReducer.jsx
@@ -5,8 +5,12 @@ const userSlice = createSlice({
     name: "users",
     initialState: UserList,
     reducers: {
-
+        addUser: (state, action) => {
+            state.push(action.payload)
+        }
     }
 })
 
+
+export const {addUser} = userSlice.actions
 export default userSlice.reducer
